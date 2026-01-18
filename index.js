c2380a10a8220a143c0c9ad5d54348a1044f9cf8
Matar Calif <matarcalif@gmail.com>
2026-01-18 19:03:17 +0200

    changed background colour

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('WASSSSSSUPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP');
+  res.send(`
+    <html>
+      <body style="background-color: #311432; color: white;">
+        <h1>WASSSSSSUPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP</h1>
+      </body>
+    </html>
+  `);
 });
 
 app.listen(PORT, () => {
